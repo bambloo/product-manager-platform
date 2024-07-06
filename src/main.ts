f3092ca8c544313b858c2f9abbb9ce4eb4646787
bambloo <liugchome2004@163.com>
2024-07-06 09:10:24 +0800

    init pinia

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { createGtm } from '@gtm-support/vue-gtm'
 import router from './router'
 import vuesticGlobalConfig from './vuestic-config'
 import App from './App.vue'
+import stores from './stores'
 
 const app = createApp(App)
 
+app.use(stores)
 app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
