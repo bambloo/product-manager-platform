9fe61677f2f65cb5f0bfbaf3aed410ffcad54d38
bambloo <liugchome2004@163.com>
2024-07-06 09:07:55 +0800

    init tailwind, postcss, fonts

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts"></script>
+<style lang="scss">
+@import 'scss/main.scss';
+
+#app {
+  font-family: 'Inter', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  margin: 0;
+  min-width: 20rem;
+}
+</style>
 <template>
+  <VaButton>test</VaButton>
   <RouterView />
 </template>
