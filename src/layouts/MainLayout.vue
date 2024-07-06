d0c7070e0769ba596e051634668373a2c4851ce7
bambloo <liugchome2004@163.com>
2024-07-06 09:23:40 +0800

    sidebar controll

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,45 @@
 import { useBreakpoint } from 'vuestic-ui/web-components'
 import { useUiStates } from '../stores/ui-states'
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
+import { Ref, computed, onBeforeUnmount, onMounted, ref } from 'vue'
+import { onBeforeRouteUpdate } from 'vue-router'
 const uiStates = useUiStates()
+const { isSidebarMinimized } = storeToRefs(uiStates)
+
 const breakpoints = useBreakpoint()
 
-const { isSidebarMinimized } = storeToRefs(uiStates)
 const sidebarWidth = ref('16rem')
-const sidebarMinimizedWidth = ref(undefined)
+const sidebarMinimizedWidth: Ref<string | undefined> = ref(undefined)
+const isMobile = ref(false)
+const isTablet = ref(false)
+
+const onResize = () => {
+  isSidebarMinimized.value = breakpoints.mdDown
+  isMobile.value = breakpoints.smDown
+  isTablet.value = breakpoints.mdDown
+  sidebarMinimizedWidth.value = isMobile.value ? '0' : '4.5rem'
+  sidebarWidth.value = isTablet.value ? '100 %' : '16rem'
+}
+
+onMounted(() => {
+  window.addEventListener('resize', onResize)
+  onResize()
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', onResize)
+})
+
+onBeforeRouteUpdate(() => {
+  if (breakpoints.mdDown) {
+    isSidebarMinimized.value = true
+  }
+})
+
+const isFullScreenSidebar = computed(() => isTablet.value && !isSidebarMinimized.value)
+const onCloseSidebarButtonClick = () => {
+  isSidebarMinimized.value = true
+}
 </script>
 
 <template>
